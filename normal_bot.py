e22641c8111bcfee3cd32d022d2a04e2fa8c8bcd
Megugo <Buhlou89@gmail.com>
2021-03-30 12:33:41 +0300

    Update normal_bot.py

--- normal_bot.py
+++ normal_bot.py
@@ -16,6 +16,7 @@ global cur_id
 global msg
 global data_c
 global time
+global check
 
 rospy.init_node(f"laser_checker_{str(sys.argv[1])}")
 pub = rospy.Publisher(f"/lidar_check_{str(sys.argv[1])}", String, queue_size=10)
@@ -30,6 +31,7 @@ preputation = [0.0,0.0,0.0]
 truth_p = [0.0,0.0]
 data_c = [3,3,3]
 time = 0
+check = [0,0]
 def laser_callback(data):
     min_range = 30
     for _range in data.ranges[300:420]:
@@ -44,53 +46,55 @@ def laser_callback(data):
     data_c[int(b_number)-1] = int(msg)
 def callback1(data):
     data_c[MAS_id[0]-1] = int(data.data)
-    #print(1)
+    check[0] = 1
+
 def callback2(data):
     data_c[MAS_id[1]-1] = int(data.data)
-    #print(2)
+    check[1] = 1
 
 rospy.Subscriber(f"/bot_{str(int(sys.argv[1])-1)}/laser/scan", LaserScan, laser_callback)
 rospy.Subscriber(f"/lidar_check_{str(MAS_id[0])}", String, callback1)
 rospy.Subscriber(f"/lidar_check_{str(MAS_id[1])}", String, callback2)
 rate = rospy.Rate(100)
 while not rospy.is_shutdown():
-    time += 1
-    #print ((data_c))
-    I0 = (int(data_c[1] == data_c[0]) + int(data_c[1] == data_c[1]) + int(data_c[1] == data_c[2]))/3
-    I1 = (int(data_c[2] == data_c[0]) + int(data_c[2] == data_c[1]) + int(data_c[2] == data_c[2]))/3
-    #rep_change(I0,I1)
-    truth_p[0] += I0
-    truth_p[1] += I1
-    #print ("I0,I1",I0,I1)
-        #data_c = []
-    print ("iteration:", time)
-    rt = [I0,I1]
-    #for i in rt:
-    if I0 >= 0.5:
-        buff = 0.75 + truth_p[0]
-        preputation[MAS_id[0]-1] = creputation[MAS_id[0]-1]
-        creputation[MAS_id[0]-1] = buff/time
-    else:
-        buff = 0.75 + truth_p[0] - (preputation[MAS_id[0]-1] - exp(-(1-I0)*time))
-        preputation[MAS_id[0]-1] = creputation[MAS_id[0]-1]
-        creputation[MAS_id[0]-1] = buff/time
-    if I1 >= 0.5:
-        buff = 0.75 + truth_p[1]
-        preputation[MAS_id[1]-1] = creputation[MAS_id[1]-1]
-        creputation[MAS_id[1]-1] = buff/time
-    else:
-        buff = 0.75 + (truth_p[1]) - (preputation[MAS_id[1]-1] - exp(-(1-I1)*time))
-        preputation[MAS_id[1]-1] = creputation[MAS_id[1]-1]
-        creputation[MAS_id[1]-1] = buff/time
-    k = 1
-    for i in creputation:
-        if i < 0.4:
-            print ("Robot", k, "imposter", i)
-            exit(1)
-        elif i == 0.75:
-            print ("Robot", k, "my rate", i)
+    if check[0] == 1 and check[1] == 1:
+        time += 1
+        #print ((data_c))
+        I0 = (int(data_c[1] == data_c[0]) + int(data_c[1] == data_c[1]) + int(data_c[1] == data_c[2]))/3
+        I1 = (int(data_c[2] == data_c[0]) + int(data_c[2] == data_c[1]) + int(data_c[2] == data_c[2]))/3
+        #rep_change(I0,I1)
+        truth_p[0] += I0
+        truth_p[1] += I1
+        #print ("I0,I1",I0,I1)
+            #data_c = []
+        print ("iteration:", time)
+        rt = [I0,I1]
+        #for i in rt:
+        if I0 >= 0.5:
+            buff = 0.75 + truth_p[0]
+            preputation[MAS_id[0]-1] = creputation[MAS_id[0]-1]
+            creputation[MAS_id[0]-1] = buff/time
         else:
-            print ("Robot", k, "OK", i)
-        k+=1
-    rate.sleep()
-    pass
+            buff = 0.75 + truth_p[0] - (preputation[MAS_id[0]-1] - exp(-(1-I0)*time))
+            preputation[MAS_id[0]-1] = creputation[MAS_id[0]-1]
+            creputation[MAS_id[0]-1] = buff/time
+        if I1 >= 0.5:
+            buff = 0.75 + truth_p[1]
+            preputation[MAS_id[1]-1] = creputation[MAS_id[1]-1]
+            creputation[MAS_id[1]-1] = buff/time
+        else:
+            buff = 0.75 + (truth_p[1]) - (preputation[MAS_id[1]-1] - exp(-(1-I1)*time))
+            preputation[MAS_id[1]-1] = creputation[MAS_id[1]-1]
+            creputation[MAS_id[1]-1] = buff/time
+        k = 1
+        for i in creputation:
+            if i < 0.4:
+                print ("Robot", k, "imposter", i)
+                exit(1)
+            elif i == 0.75:
+                print ("Robot", k, "my rate", i)
+            else:
+                print ("Robot", k, "OK", i)
+            k+=1
+        rate.sleep()
+        pass
